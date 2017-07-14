0fb42a6b0bd051e97c25b5e77fa7162412c3e389
brandinesa <meinpatrick@gmx.at>
2017-07-14 16:05:14 +0200

    Initial project setup

---
+++ src/main/java/inc/alc/beercounter/BeercounterApplication.java
@@ -0,0 +1,12 @@
+package inc.alc.beercounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeercounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeercounterApplication.class, args);
+	}
+}
---
+++ src/test/java/inc/alc/beercounter/BeercounterApplicationTests.java
@@ -0,0 +1,16 @@
+package inc.alc.beercounter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BeercounterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
